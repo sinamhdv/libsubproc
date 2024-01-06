f502d53bb73e479df748dbe7be17b4c8b16ae9ec
sinamhdv <sinamhdv@gmail.com>
2024-01-05 23:05:28 -0800

    moved docs for sp_open() into the headers

--- include/subproc/subproc.h
+++ include/subproc/subproc.h
@@ -15,4 +15,17 @@ typedef struct
 	pid_t pid;
 } subproc;
 
+/**
+ * @brief Opens a new subprocess
+ * 
+ * @param executable the executable file's name
+ * @param argv
+ * @param envp
+ * @param fd_in stdin of the child: either SPIO_PIPE, SPIO_PTY, or a file descriptor
+ * @param fd_out stdout of the child
+ * @param fd_err stderr of the child
+ * @return a pointer to the subproc object containing information about the created child
+ */
+subproc *sp_open(char *executable, char *argv[], char *envp[], int fd_in, int fd_out, int fd_err);
+
 #endif	// HEADER_SUBPROC_H
--- src/process.c
+++ src/process.c
@@ -100,17 +100,6 @@ fail:
 	return -1;
 }
 
-/**
- * @brief Opens a new subprocess
- * 
- * @param executable the executable file's name
- * @param argv
- * @param envp
- * @param fd_in stdin of the child: either SPIO_PIPE, SPIO_PTY, or a file descriptor
- * @param fd_out stdout of the child
- * @param fd_err stderr of the child
- * @return a pointer to the subproc object containing information about the created child
- */
 subproc *sp_open(char *executable, char *argv[], char *envp[], int fd_in, int fd_out, int fd_err)
 {
 	// allocate the struct
