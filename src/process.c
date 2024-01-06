38d87dbe074587784e8e732e659b1c26858fa3f8
sinamhdv <sinamhdv@gmail.com>
2024-01-05 22:59:25 -0800

    moved pty creation code into a seperate function

--- src/process.c
+++ src/process.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <sys/ioctl.h>
 #include "subproc/subproc.h"
 
 /**
@@ -12,44 +13,22 @@
  * 
  * @param fd_status an array showing what we should do for each standard fd
  * @param used_fds the fd array to fill for stdin/stdout/stderr and read/write end
+ * @param pty_master the master side of the created pty, if used
  * @return true on success, false on error
  */
-static bool create_used_fds(int fd_status[3], int used_fds[3][2])
+static bool create_used_fds(int fd_status[3], int used_fds[3][2], int pty_master)
 {
-	int pty_master = -1;
-
 	for (int i = 0; i < 3; i++)
 	{
 		if (fd_status[i] == SPIO_PTY)
-		{
-			if (pty_master == -1)
-			{
-				if ((pty_master = posix_openpt(O_RDWR | O_NOCTTY)) == -1)
-					return false;
-				if (grantpt(pty_master) == -1)
-				{
-					close(pty_master);
-					return false;
-				}
-				if (unlockpt(pty_master) == -1)
-				{
-					close(pty_master);
-					return false;
-				}
-				if (fcntl(pty_master, F_SETFD, FD_CLOEXEC) == -1)
-					return false;
-				used_fds[i][0] = used_fds[i][1] = pty_master;
-			}
-		}
+			used_fds[i][0] = used_fds[i][1] = pty_master;
 		else if (fd_status[i] == SPIO_PIPE)
 		{
 			if (pipe2(used_fds[i], O_CLOEXEC) == -1)
 				return false;
 		}
 		else
-		{
 			used_fds[i][0] = used_fds[i][1] = fd_status[i];
-		}
 	}
 
 	return true;
@@ -68,17 +47,46 @@ static bool duplicate_used_fds(int fd_status[3], int used_fds[3][2])
 	{
 		if (fd_status[i] == SPIO_PIPE)
 		{
-
+			if (dup2(used_fds[i][(i == 0 ? 0 : 1)], i) == -1)
+				return false;
 		}
 		else if (fd_status[i] == SPIO_PTY)
 		{
-
+			char slave_name[256];
+			if (ptsname_r(used_fds[i][0], slave_name, sizeof(slave_name)))
+				return false;
+			int pty_slave = open(slave_name, O_RDWR | O_CLOEXEC);
+			if (pty_slave == -1) return false;
+			set_tty_options(pty_slave);
+			if (setsid() == -1)
+				return false;
+			if (ioctl(pty_slave, TIOCSCTTY, 0) == -1)
+				return false;
+			dup2()
 		}
 		else
 		{
 			
 		}
 	}
+}
+
+static int create_pty_master(void)
+{
+	int pty_master;
+	if ((pty_master = posix_openpt(O_RDWR | O_NOCTTY)) == -1)
+		return false;
+	if (grantpt(pty_master) == -1)
+		goto fail;
+	if (unlockpt(pty_master) == -1)
+		goto fail;
+	if (fcntl(pty_master, F_SETFD, FD_CLOEXEC) == -1)
+		goto fail;
+	
+	return pty_master;
+fail:
+	close(pty_master);
+	return -1;
 }
 
 /**
@@ -105,6 +113,13 @@ subproc *sp_open(char *executable, char *argv[], char *envp[], int fd_in, int fd
 
 	int fd_status[3] = {fd_in, fd_out, fd_err};
 	int used_fds[3][2];
+	bool use_pty = (fd_status[0] == SPIO_PTY ||
+					fd_status[1] == SPIO_PTY ||
+					fd_status[2] == SPIO_PTY);
+	int pty[2] = {-1, -1};	// master/slave
+
+	if (use_pty && !create_pty(pty))
+		goto fail;
 	if (!create_used_fds(fd_status, used_fds))
 		goto fail;
 
