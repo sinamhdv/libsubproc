a084f84e85b657984abf50487cebd4de066e0ecd
sinamhdv <sinamhdv@gmail.com>
2024-07-03 16:03:03 -0700

    move seterror macro to errors.h

--- include/subproc/errors.h
+++ include/subproc/errors.h
@@ -1,6 +1,11 @@
 #ifndef SP_HEADER_ERRORS_H
 #define SP_HEADER_ERRORS_H
 
+#define seterror(errfunc, action) { \
+			sp_errno = errno; \
+			sp_errfunc = (errfunc); \
+			action; }
+
 extern int sp_errno;	// last library errno we faced in sp functions
 extern char *sp_errfunc;	// name of the library function that caused this errno
 
